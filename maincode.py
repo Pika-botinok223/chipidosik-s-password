c27c282595160db9e9cec0008c7c503c695b991f
Pika-botinok223 <81578937+Pika-botinok223@users.noreply.github.com>
2022-04-05 16:59:04 +0300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,1 +1,6 @@
-
+from scratchclient import ScratchSession
+sc = ScratchSession ("clyde_bot_ds", "sleepingdinosaur2016")
+d = ["chipidosik"]
+for user in d:
+  s.getuser[d].password
+  print{u.profile.password}
